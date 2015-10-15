6e9246ebbcb1c05cffb5819f895f125b30c8a277
Jennifer Owens <jmo.jennifer@gmail.com>
2015-10-15 14:40:39 -0700

    Essay poem
    Poem I wrote for my Winter 2016 Ada Developers Academy Application

---
+++ my_dream_poem_ada_app_winter_2016.rb
@@ -0,0 +1,15 @@
+jennifer_owens = 'My'
+fifth_cohort  = 'dream'
+third_cohort = 'Second time applied'
+fell_short = 'So close, yet so far'
+optimism_for_future = 'Gonna keep trying'
+
+puts ' '
+puts jennifer_owens + ' ' + fifth_cohort
+puts ' '
+puts third_cohort
+puts fell_short
+puts optimism_for_future
+puts ' '
+puts 'Thank you for considering my application! :)'
+puts ' '
