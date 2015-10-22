46dab7da03aed15eb5724b929a4d1af762668dca
Jennifer Owens <jmo.jennifer@gmail.com>
2015-10-21 22:34:34 -0700

    new poem + ss
    conTEST the CONtest poem, with output screenshot

---
+++ contest.rb
@@ -0,0 +1,51 @@
+
+puts ' '
+puts 'conTEST the CONtest'
+
+doubt = 'Sinking suspicion starts small'
+increases = 'then grows;'
+a_crescendo = 'soul sucking black hole.'
+
+puts ' '
+puts doubt + ' ' + increases
+puts a_crescendo
+puts 'Don\'t. let. it. win.'
+puts ' '
+
+just = 'You\'re more than that'
+breathe = 'so much more than that'
+fight = 'and if you settle for that,'
+back = 'who wins?'
+
+
+puts just 
+puts breathe
+puts fight
+puts back
+puts ' '
+
+
+imposter = 'Remember all your efforts.'
+syndrome = 'Remember you\'re amazing.'
+go_to = 'Remember that and silence'
+hell = 'voices outside, voices in.'
+
+puts imposter
+puts syndrome
+puts go_to
+puts hell
+puts ' '
+
+mind = ['hopes', 'ideas', 'vision', 'options']
+
+puts 'Keep your ' 
+mind.each do |positivity|
+    puts "#{positivity} going strong, and"
+end
+puts 'turnabout the doubt.'
+
+puts ' '
+puts '\'Cause when they say WTF?' 
+
+puts 'It\'s ' + 'WTF'.reverse! + '.'
+puts ' '
